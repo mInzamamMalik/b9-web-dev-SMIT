46981bf922741831c5cbfa1b82748fa2847541ba
M Inzamam Malik <malikasinger1@gmail.com>
2023-06-19 21:04:28 +0500

    class code

--- 4. server/2. expressJs hello world/server.mjs
+++ 4. server/2. expressJs hello world/server.mjs
@@ -1,16 +1,51 @@
 import express from 'express';
+import cors from 'cors';
+import path  from 'path';
+const __dirname = path.resolve();
 
 const app = express();
+// app.use(cors())
 
-// http://192.168.210.35:3000/profile
-app.get('/', (req, res) => {
-    console.log('Hello World!', new Date());
-    res.send('Hello World!' + new Date());
-})
+
 app.get('/profile', (req, res) => {
     console.log('this is profile!', new Date());
     res.send('this is profile' + new Date());
 })
+app.get('/weather/:cityName', (req, res) => {
+    console.log('this is profile!', new Date());
+
+    let weatherData = {
+        karachi: {
+            city: 'karachi',
+            tempInC: 30,
+            humidity: 56,
+            high: 32,
+            low: 18
+        },
+        london: {
+            city: 'london',
+            tempInC: 5,
+            humidity: 56,
+            high: 32,
+            low: 18
+        },
+    }
+
+    let userInputCityName = req.params.cityName.toLowerCase();
+    console.log("userInputCityName: ", userInputCityName);
+
+    let weatherDataToSend = weatherData[userInputCityName];
+
+    if (weatherDataToSend) {
+        res.send(weatherDataToSend);
+    } else {
+        res.status(404)
+            .send(`weather data is not available for ${req.params.cityName}`);
+    }
+})
+
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
