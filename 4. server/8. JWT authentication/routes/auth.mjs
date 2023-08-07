6af89ed772637d7de6a081c4f3880e3549065c8e
M Inzamam Malik <malikasinger1@gmail.com>
2023-08-07 22:14:45 +0500

    class code

--- 4. server/8. JWT authentication/routes/auth.mjs
+++ 4. server/8. JWT authentication/routes/auth.mjs
@@ -2,58 +2,59 @@
 import express from 'express';
 let router = express.Router()
 import { client } from './../mongodb.mjs'
+import { 
+    stringToHash,
+    varifyHash 
+} from "bcrypt-inzi"
 
 const userCollection = client.db("cruddb").collection("users");
 
-router.post('/login', (req, res, next) => {
-    console.log('this is login!', new Date());
-    res.send('this is login v1' + new Date());
-})
-router.post('/signup', async (req, res, next) => {
+router.post('/login', async (req, res, next) => {
 
     if (
-        !req.body?.firstName
-        || !req.body?.LastName // family name, sur name
-        || !req.body?.email
+        !req.body?.email
         || !req.body?.password
     ) {
         res.status(403);
         res.send(`required parameters missing, 
         example request body:
         {
-            firstName: "some firstName",
-            LastName: "some LastName",
             email: "some@email.com",
             password: "some$password",
         } `);
         return;
     }
-
     req.body.email = req.body.email.toLowerCase();
-
 
     try {
         let result = await userCollection.findOne({ email: req.body.email });
         console.log("result: ", result);
 
         if (!result) { // user not found
-
-            const insertResponse = await userCollection.insertOne({
-                firstName: req.body.firstName,
-                LastName: req.body.LastName,
-                email: req.body.email,
-                password: req.body.password, // TODO: convert password into hash
-                createdOn: new Date()
-            });
-            console.log("insertResponse: ", insertResponse);
-
-            res.send({ message: 'Signup successful' });
-
-        } else { // user already exists
             res.status(403).send({
-                message: "user already exist with this email",
-                abc: "user already exist with this email"
+                message: "email or password incorrect"
             });
+            return;
+        } else { // user found
+
+
+            const isMatch = await varifyHash(req.body.password, result.password)
+
+            if (isMatch) {
+                
+                
+
+                // TODO: create token for this user
+                res.send({
+                    message: "login successful"
+                });
+                return;
+            } else {
+                res.status(401).send({
+                    message: "email or password incorrect"
+                })
+                return;
+            }
         }
 
     } catch (e) {
@@ -63,11 +64,60 @@ router.post('/signup', async (req, res, next) => {
 
 
 
+})
+router.post('/signup', async (req, res, next) => {
+
+    if (
+        !req.body?.firstName
+        || !req.body?.lastName // family name, sur name
+        || !req.body?.email
+        || !req.body?.password
+    ) {
+        res.status(403);
+        res.send(`required parameters missing, 
+        example request body:
+        {
+            firstName: "some firstName",
+            lastName: "some lastName",
+            email: "some@email.com",
+            password: "some$password",
+        } `);
+        return;
+    }
+
+    req.body.email = req.body.email.toLowerCase();
+    // TODO: validate email
 
 
+    try {
+        let result = await userCollection.findOne({ email: req.body.email });
+        console.log("result: ", result);
 
+        if (!result) { // user not found
 
+            const passwordHash = await stringToHash(req.body.password);
 
+            const insertResponse = await userCollection.insertOne({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: passwordHash,
+                createdOn: new Date()
+            });
+            console.log("insertResponse: ", insertResponse);
+
+            res.send({ message: 'Signup successful' });
+
+        } else { // user already exists
+            res.status(403).send({
+                message: "user already exist with this email"
+            });
+        }
+
+    } catch (e) {
+        console.log("error getting data mongodb: ", e);
+        res.status(500).send('server error, please try later');
+    }
 })
 
 
