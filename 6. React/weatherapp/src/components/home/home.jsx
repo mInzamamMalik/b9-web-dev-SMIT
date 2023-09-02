df437d2a92786a811efe7fd1045e684d00d85684
M Inzamam Malik <malikasinger1@gmail.com>
2023-09-02 15:13:14 +0500

    class code

--- 6. React/weatherapp/src/components/home/home.jsx
+++ 6. React/weatherapp/src/components/home/home.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import WeatherCard from "../weatherWidget/weatherWidget";
 
@@ -9,21 +9,67 @@ const Home = () => {
   const [weatherData, setWeatherData] = useState([]);
   const cityNameRef = useRef(null);
 
+  const [currentLocationWeather, setCurrentLocationWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const controller = new AbortController();
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (location) => {
+        console.log("location: ", location);
+
+        try {
+          let API_KEY = "e0f99c494c2ce394a18cc2fd3f100543";
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${API_KEY}&units=metric`,
+            {
+              signal: controller.signal,
+            }
+          );
+          console.log(response.data);
+
+          setCurrentLocationWeather(response.data);
+          setIsLoading(false);
+        } catch (error) {
+          console.log(error.data);
+          setIsLoading(false);
+        }
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+
+    return () => {
+      // cleanup function
+      controller.abort();
+    };
+  }, []);
+
   const submitHandler = async (e) => {
     e.preventDefault();
     console.log("cityName: ", cityNameRef.current.value);
 
     let API_KEY = "e0f99c494c2ce394a18cc2fd3f100543";
     try {
+      setIsLoading(true);
+
       const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityNameRef.current.value}&appid=${API_KEY}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityNameRef.current.value}&appid=${API_KEY}&units=metric`,
+        {
+          signal: controller.signal,
+        }
       );
 
       console.log(response.data);
       setWeatherData([response.data, ...weatherData]);
+      setIsLoading(false);
     } catch (error) {
       // handle error
-      console.log(error.data);
+      console.log(error?.data);
+      setIsLoading(false);
     }
   };
 
@@ -46,13 +92,15 @@ const Home = () => {
 
       <hr />
 
-      {weatherData.length ? (
-        weatherData.map((eachWeatherData, index) => {
-          return <WeatherCard key={index} weatherData={eachWeatherData} />;
-        })
-      ) : (
-        <div>No Data</div>
-      )}
+      {isLoading ? <div>Loading...</div> : null}
+
+      {weatherData.length || currentLocationWeather || isLoading ? null : <div>No Data</div>}
+
+      {weatherData.map((eachWeatherData, index) => {
+        return <WeatherCard key={index} weatherData={eachWeatherData} />;
+      })}
+
+      {currentLocationWeather ? <WeatherCard weatherData={currentLocationWeather} /> : null}
     </div>
   );
 };
